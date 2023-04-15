40d708231b10109aff5055901696aff06333a891
chacharin <102975545+chacharin@users.noreply.github.com>
2023-04-15 23:51:35 +0700

    Update ROS_GUI_2-Scale.py

--- ROS_GUI_2-Scale.py
+++ ROS_GUI_2-Scale.py
@@ -9,15 +9,11 @@ frame.geometry("200x200")
 S1_value = 0
 S2_value = 0
 
-
 rospy.init_node('GUI')
 pub1 = rospy.Publisher('Topic_servo_9', Int16, queue_size=10)
 pub2 = rospy.Publisher('Topic_servo_10', Int16, queue_size=10)
 rate = rospy.Rate(100)
 rate.sleep()
-
-def publish():
-    talker()
 
 def talker(val):
     global S1_value
@@ -29,12 +25,12 @@ def talker(val):
     pub1.publish(S1_value)
     pub2.publish(S2_value)
 
-S1= Scale(frame, from_=0, to=180, lable= "SV-9", command=talker)
-S1.place(x=10, y=10)
+S1= Scale(frame, from_=0, to=180, label= "SV-9", command=talker)
+S1.place(x=5, y=10)
 S1.set(90)
 
-S2= Scale(frame, from_=0, to=180, lable= "SV-10", command=talker)
-S2.place(x=70, y=10)
+S2= Scale(frame, from_=0, to=180, label= "SV-10", command=talker)
+S2.place(x=100, y=10)
 S2.set(90)
 
 frame.mainloop()
