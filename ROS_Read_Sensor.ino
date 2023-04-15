bac52d99b2681a6d97f271a7c240e9ae3dd44f04
chacharin <102975545+chacharin@users.noreply.github.com>
2023-04-16 00:28:51 +0700

    Add files via upload

---
+++ ROS_Read_Sensor.ino
@@ -0,0 +1,20 @@
+#include <ros.h>
+#include <std_msgs/Int16.h>
+
+ros::NodeHandle nh; 
+std_msgs::Int16 sensorData;
+ros::Publisher pub("Topic_Sensor", &sensorData);
+
+void setup() 
+{
+   pinMode(3,INPUT);
+   nh.initNode();
+   nh.advertise(pub);
+}
+ 
+void loop() {
+    sensorData.data = digitalRead(3);
+    pub.publish(&sensorData);
+    delay(1);
+    nh.spinOnce();
+}
