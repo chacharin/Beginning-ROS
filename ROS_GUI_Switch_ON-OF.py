b4cb649ebb7d6ecc96a9aaf74e23b2af806a06c8
chacharin <102975545+chacharin@users.noreply.github.com>
2023-04-15 22:37:13 +0700

    Update ROS_GUI_Switch_ON-OF.py

--- ROS_GUI_Switch_ON-OF.py
+++ ROS_GUI_Switch_ON-OF.py
@@ -6,19 +6,19 @@ from std_msgs.msg import Int16
 frame=Tk()
 frame.geometry("200x200")
 
-
+rospy.init_node('GUI_LED_Control', anonymous=True)
+rate = rospy.Rate(10) 
+rate.sleep()
+pub1 = rospy.Publisher('Topic_LED_13', Int16, queue_size=10)
+   
 def publish():
     talker()
 
 def talker(val):
-    pub1 = rospy.Publisher('Topic_LED_13', Int16, queue_size=10)
-    rospy.init_node('GUI_LED_Control', anonymous=True)
-    rate = rospy.Rate(10) 
     cmd_value = Int16(val)
     rospy.loginfo(cmd_value)
     pub1.publish(cmd_value)
-    rate.sleep()
-
+    
 B1= Button(frame,text = "ON",command= lambda: talker(1))
 B1.pack()
 B2= Button(frame,text = "OFF",command= lambda: talker(0))
