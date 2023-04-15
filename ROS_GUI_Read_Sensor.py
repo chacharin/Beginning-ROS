a65ccbcd791456c484d19f3fa70ca78ffae3d40f
chacharin <102975545+chacharin@users.noreply.github.com>
2023-04-16 00:59:18 +0700

    Add files via upload

---
+++ ROS_GUI_Read_Sensor.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from tkinter import*
+import rospy
+from std_msgs.msg import Int16
+
+frame=Tk()
+frame.geometry("200x200")
+
+rospy.init_node('GUI')
+rate = rospy.Rate(10) 
+rate.sleep()
+
+def read(num):
+    sensor_read = num.data
+    rospy.loginfo(str(sensor_read))
+    L1.config(text=str(sensor_read))
+
+sub = rospy.Subscriber('Topic_Sensor', Int16, callback= read)
+    
+L1= Label(frame,font=('Arial',40), text = "0")
+L1.pack()
+
+frame.mainloop()
