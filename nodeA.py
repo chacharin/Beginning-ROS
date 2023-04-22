e8cfbd57fa490c8f23bf9c878310536c728548d6
chacharin <102975545+chacharin@users.noreply.github.com>
2023-04-22 20:37:42 +0700

    Add files via upload

---
+++ nodeA.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import String
+
+text = "hi"
+
+if __name__ == "__main__":
+    pub = rospy.Publisher("chatter",String,queue_size=10)
+    rospy.init_node("Talker")
+    rate = rospy.Rate(0.5)
+    while(not rospy.is_shutdown()):
+        pub.publish(text)
+        rospy.loginfo("Node1: " + str(text))
+        rate.sleep()
+    
